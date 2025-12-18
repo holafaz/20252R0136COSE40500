cacecb35b00b379fcd75162c6b7ac2fbbe8b57f2
holafaz <fazrulazim@gmail.com>
2025-12-18 12:32:00 +0900

    Implement basic Queue class with enqueue/dequeue

---
+++ queue.py
@@ -0,0 +1,19 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        return None
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+print(q.dequeue())
